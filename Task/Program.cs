07505bdcacc286c55337352afde52b7fce1307bb
Evgeniy <jon83@inbox.ru>
2022-05-10 00:40:56 +0300

    add Task version 1

---
+++ Task/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = new string[] {"hello", "2", "world", ":-)"};
+int count = 0;
+int len = array.Length; 
+for (int index = 0; index < len; index++)
+{
+    if (array[index].Length <= 3)
+    {
+        array[count++] = array[index];
+    }
+}
+Array.Resize(ref array, count);
+Console.WriteLine("[\"{0}\"]",string.Join("\", \"", array));
